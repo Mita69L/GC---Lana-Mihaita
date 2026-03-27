ef93f05e143ebe48445f90ca9e9b6bfa7bd0f8b9
MihaitaL <mihai.lana2000@gmail.com>
2026-03-28 00:39:32 +0200

    Add files via upload

---
+++ Laboratorul 03/src/main/java/cg/bouncysquare/BouncySquareActivity.java
@@ -0,0 +1,27 @@
+package cg.bouncysquare;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class BouncySquareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        GLSurfaceView view = new GLSurfaceView(this);
+
+        view.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+
+        view.setRenderer(new SquareRenderer());
+
+        setContentView(view);
+    }
+}
